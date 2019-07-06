fa4b0edbd8b1c962d6be8dfe31650aa16ee3e9c7
Ruslan <platonpopelishin@gmail.com>
2019-07-06 16:57:26 +0300

    can update answers

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,10 +2,13 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
-use App\Question;
+
 use App\Policies\QuestionPolicy;
+use App\Policies\AnswerPolicy;
+
+use App\Question;
+use App\Answer;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -16,6 +19,7 @@ class AuthServiceProvider extends ServiceProvider
      */
     protected $policies = [
         Question::class => QuestionPolicy::class,
+        Answer::class => AnswerPolicy::class,
     ];
 
     /**
