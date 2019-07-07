e520cb4630fe84b46b154b8540f05533d7f96b08
Loafer19 <platonpopelishin@gmail.com>
2019-07-07 15:06:01 +0300

    Implement accept the answer as best answer

--- resources/views/answers/_index.blade.php
+++ resources/views/answers/_index.blade.php
@@ -20,9 +20,24 @@
                                 <i class="fas fa-caret-down fa-3x"></i>
                             </a>
 
-                            <a href="" title="Mark this answer as best answer" class="{{ $answer->status }} mt-2">
-                                <i class="fas fa-check fa-2x"></i>
-                            </a>
+                            @can ('accept', $answer)
+                                <a href="" title="Mark this answer as best answer"
+                                    class="{{ $answer->status }} mt-2"
+                                    onclick="event.preventDefault(); document.getElementById('accept-answer-{{ $answer->id }}').submit();"
+                                    >
+                                    <i class="fas fa-check fa-2x"></i>
+                                </a>
+                                <form action="{{ route('answers.accept', $answer->id) }}" method="post" id="accept-answer-{{ $answer->id }}" style="display:none">
+                                    @csrf
+                                </form>
+                            @else
+                                @if ($answer->is_best)
+                                <a href="" title="The question owner accepted this answer as the best answer"
+                                    class="{{ $answer->status }} mt-2">
+                                    <i class="fas fa-check fa-2x"></i>
+                                </a>
+                                @endif
+                            @endcan
                         </div>
                         <div class="media-body">
                             {!! $answer->body_html !!}
